ab96d50e697f18d9fea87ee86770efa7fe9c5742
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2025-05-21 19:01:35 +0700

    add: bg

--- React Realm/src/App.jsx
+++ React Realm/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
