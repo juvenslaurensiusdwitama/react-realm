1c05df6024e711dd5bc392d079d39d4fea8e9a5d
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2025-05-29 17:40:38 +0700

    feat: 404 page

--- React Realm/src/components/NotFound.jsx
+++ React Realm/src/components/NotFound.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import troll404 from '../assets/image404.png'
 import bg404 from '../assets/bg404.jpg'
+import backToHome from '../assets/back home.png'
 
 const NotFound = () => {
     const navigate = useNavigate()
@@ -9,11 +10,11 @@ const NotFound = () => {
         className="flex justify-center items-center h-screen bg-cover bg-center"
         style={{ backgroundImage: `url(${bg404})` }}
     >
-        <div className='flex flex-col items-center text-[#633a1c] font-semibold text-[36px]'>
-            <p className=''>Error 404</p>
-            <img src={troll404} alt="" className='h-[450px]'/>
-            <p>Page not found</p>
-            <button onClick={() => navigate('/')} className='cursor-pointer'>Back</button>
+        <div className='flex flex-col items-center gap-1'>
+            <img src={troll404} alt="" className='h-[450px] mt-10'/>
+            <div className='flex gap-4 items-center'>
+              <img src={backToHome} onClick={() => navigate('/')} className='cursor-pointer h-[50px]'/>
+            </div>
         </div>
     </div>
   )
