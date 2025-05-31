f1f4d10c8689e60f22bcee885a4354dd08cb41fa
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2025-06-01 02:32:28 +0700

    .

--- React Realm/src/pages/Quest/components/Lesson.jsx
+++ React Realm/src/pages/Quest/components/Lesson.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import Menu from '../../../components/Menu';
+import bgQuest from '../../../assets/bg-quest.png'
 
 const Lesson = ({ data, loading }) => {
     if (loading) return <div>Loading...</div>;
     console.log(data)
     return (
-        <div>
+        <div
+            className="h-screen bg-cover bg-center flex flex-col items-center"
+            style={{ backgroundImage: `url(${bgQuest})` }}
+        >
+            <Menu />
             <h1>{data?.type}</h1>
         </div>
     )
--- React Realm/src/pages/Quest/components/Quiz.jsx
+++ React Realm/src/pages/Quest/components/Quiz.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import bgQuest from '../../../assets/bg-quest.png'
+import Menu from '../../../components/Menu';
 
 const Quiz = ({ data, loading }) => {
     if (loading) return <div>Loading...</div>;
     console.log(data)
     return (
-        <div>
+        <div
+            className="h-screen bg-cover bg-center flex flex-col items-center"
+            style={{ backgroundImage: `url(${bgQuest})` }}
+        >
+            <Menu />
             <h1>{data?.type}</h1>
         </div>
     )
