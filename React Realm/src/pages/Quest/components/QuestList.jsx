b28dfb0f3074b1dea4395f586c8b33d047f964d9
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2025-05-31 20:04:19 +0700

    feat: quest list api

--- React Realm/src/pages/Quest/components/QuestList.jsx
+++ React Realm/src/pages/Quest/components/QuestList.jsx
@@ -2,78 +2,58 @@ import arrow from '../../../assets/next-icon.png'
 import bronzeThropy from '../../../assets/bronze24.png'
 import silverThropy from '../../../assets/silver24.png'
 import goldThropy from '../../../assets/gold24.png'
-const QuestList = () => {
+import { collection, getDocs, query, orderBy } from "firebase/firestore";
+import { db } from '../../../config/firestore'
+import { useEffect, useState } from 'react';
 
-    const listQuest = [
-        {
-            title: 'Introduction to JSX',
-            points: null,
-            exp: 100,
-            badges: 'badge',
-            thropy: null
-        },
-        {
-            title: 'Introduction to JSX (Quiz)',
-            points: 100,
-            exp: 100,
-            badges: 'badge',
-            thropy: bronzeThropy,
-        },
-        {
-            title: 'What is React Components?',
-            points: null,
-            exp: 100,
-            badges: 'badge',
-            thropy: null
-        },
-        {
-            title: 'What is React Components? (Quiz)',
-            points: 100,
-            exp: 100,
-            badges: 'badge',
-            thropy: silverThropy,
-        },
-        {
-            title: 'How to use React Component?',
-            points: null,
-            exp: 100,
-            badges: 'badge',
-            thropy: null
-        },
-        {
-            title: 'How to use React Component? (Quiz)',
-            points: 100,
-            exp: 100,     
-            badges: 'badge',      
-            thropy: goldThropy,
-        },
-    ]
+const QuestList = () => {
+    const [questList, setQuestList] = useState()
+    const getQuestList = async() =>{
+        try{
+            const q = query(collection(db, "questList"), orderBy("exp", "asc"))
+            const querySnapshot = await getDocs(q)
+            const quests = querySnapshot.docs.map(doc => ({id: doc.id, ...doc.data()}))
+            setQuestList(quests)
+        }catch(err){
+            console.error(err)
+        }
+    }
+
+    useEffect(()=>{
+        getQuestList()
+    },[])
+    console.log(questList)
 
     return (
         <div className='my-8 flex items-center text-[#F6F8D5]'>
             <div className='bg-[#205781] rounded-[16px] border-[10px] border-[#F6F8D5] border-double
-            min-w-[750px] flex flex-col gap-4 justify-center pt-7 pb-8 px-8
+            min-w-[650px] flex flex-col gap-4 justify-center pt-7 pb-8 px-8
         '>
                 <div>
                     <h1 className='text-[32px] font-medium'>Quest List</h1>
                     <p>3 lessons • 3 quizzes</p>
                 </div>
                 <ul className='flex flex-col gap-4 font-semibold'>
-                    {listQuest.map((quest, i) => (
-                        <li className='bg-[#4F959D] p-4 rounded-[10px] flex justify-between border-[3px] 
+                    {questList?.map((quest, i) => (
+                        
+                        <li key={i} className='bg-[#4F959D] p-4 rounded-[10px] flex justify-between border-[3px] 
                             border-[#F6F8D5] border-dashed hover:bg-[#74b6a0] cursor-pointer transition 
                             duration-200'
                         >
                             <h1>{quest.title}</h1>
                             <div className='flex items-center gap-4'>
                                 <div className='flex items-center gap-1'>
-                                    <p className={quest.points === null ? 'hidden' : null}>{quest.points}pt</p>
-                                    <p>{quest.points === null ? null : '•'}</p>
+                                    <p>{quest.points? quest.points + 'pt' : null}</p>
+                                    <p>{quest.points ? '•' : null}</p>
                                     <p>{quest.exp}exp</p>
                                     <p>•</p>
                                     <p>{quest.badges}</p>
-                                    <p>{quest.thropy === null ? null : '•'}</p>
-                                    <img src={quest.thropy} alt="" className={quest.thropy === null ? 'hidden' : null}/>
+                                    <p>{quest.thropy ? '•' : null}</p>
+                                    <img src={
+                                        quest.thropy === "bronzeThropy" ? bronzeThropy 
+                                        : quest.thropy === "silverThropy" ? silverThropy 
+                                        : quest.thropy === "goldThropy" ? goldThropy : null 
+                                    } alt=""/>
                                 </div>
                                 <img src={arrow} alt="arrow" className='h-[24px] ' />
                             </div>
