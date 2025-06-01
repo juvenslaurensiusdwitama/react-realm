f2b5df77e4e32e563eb9be1d20338fdab52c7c58
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2025-06-01 15:41:00 +0700

    feat: quest list ui & api completed

--- React Realm/src/pages/Quest/components/QuestList.jsx
+++ React Realm/src/pages/Quest/components/QuestList.jsx
@@ -2,6 +2,12 @@ import arrow from '../../../assets/next-icon.png'
 import bronzeThropy from '../../../assets/bronze24.png'
 import silverThropy from '../../../assets/silver24.png'
 import goldThropy from '../../../assets/gold24.png'
+import noviceCoder from '../../../assets/noviceCoder24.png'
+import reactEnthusiast from '../../../assets/reactEnthusiast24.png'
+import jsxDebugger from '../../../assets/jsxDebugger24.png'
+import jsxArchitect from '../../../assets/jsxArchitect24.png'
+import componentMaster from '../../../assets/componentMaster24.png'
+import componentExpert from '../../../assets/componentExpert24.png'
 import { collection, getDocs, query, orderBy } from "firebase/firestore";
 import { db } from '../../../config/firestore'
 import { useEffect, useState } from 'react';
@@ -32,7 +38,7 @@ const QuestList = () => {
     return (
         <div className='my-8 flex items-center text-[#F6F8D5]'>
             <div className='bg-[#205781] rounded-[16px] border-[10px] border-[#F6F8D5] border-double
-            min-w-[656px] min-h-[280px] flex flex-col gap-4 items-center pt-7 pb-8 px-8
+            min-w-[610px] min-h-[280px] flex flex-col gap-4 items-center pt-7 pb-8 px-8
         '>
                 <div className='flex flex-col items-start w-full'>
                     <h1 className='text-[32px] font-medium'>Quest List</h1>
@@ -43,25 +49,33 @@ const QuestList = () => {
                     {isLoading && questList ? null : questList?.map((quest, i) => (
                         <li key={i} className='bg-[#4F959D] p-4 rounded-[10px] flex justify-between border-[3px] 
                             border-[#F6F8D5] border-dashed hover:bg-[#74b6a0] cursor-pointer transition 
-                            duration-200 gap-14'
+                            duration-200 gap-14 items-center'
                             onClick={() => navigate(quest.id)}
                         >
                             <h1>{quest.title}</h1>
                             <div className='flex items-center gap-4'>
                                 <div className='flex items-center gap-1'>
-                                    <p>{quest.points? quest.points + 'pt' : null}</p>
+                                    <p className='text-[14px] text-[#5decff]'>{quest.points? quest.points + ' pt' : null}</p>
                                     <p>{quest.points ? '•' : null}</p>
-                                    <p>{quest.exp}exp</p>
+                                    <p className='text-[14px] text-[#6bf559]'>{quest.exp} exp</p>
                                     <p>•</p>
-                                    <p>{quest.badges}</p>
+                                    <img src={
+                                        quest.badges === "Novice Coder" ? noviceCoder 
+                                        : quest.badges === "React Enthusiast" ? reactEnthusiast 
+                                        : quest.badges === "JSX Debugger" ? jsxDebugger 
+                                        : quest.badges === "JSX Architect" ? jsxArchitect
+                                        : quest.badges === "Component Master" ? componentMaster
+                                        : quest.badges === "Component Expert" ? componentExpert
+                                        : null
+                                    } alt="badges"/>
                                     <p>{quest.thropy ? '•' : null}</p>
                                     <img src={
                                         quest.thropy === "bronzeThropy" ? bronzeThropy 
                                         : quest.thropy === "silverThropy" ? silverThropy 
                                         : quest.thropy === "goldThropy" ? goldThropy : null 
-                                    } alt=""/>
+                                    } alt="" className='h-[18px]'/>
                                 </div>
-                                <img src={arrow} alt="arrow" className='h-[24px] ' />
+                                <img src={arrow} alt="arrow" className='h-[18px] ' />
                             </div>
                         </li>
                     ))}
