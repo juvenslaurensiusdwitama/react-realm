8d7037ab46a48daf62bc84c97f9350062d814678
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2025-06-04 00:36:54 +0700

    feat: auth ui completed

--- React Realm/src/App.jsx
+++ React Realm/src/App.jsx
@@ -5,16 +5,22 @@ import Shop from './pages/Shop/Shop'
 import Quest from './pages/Quest/Quest'
 import NotFound from './components/NotFound'
 import QuestRoutes from './pages/Quest/components/QuestRoutes'
+import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
 
 function App() {
 
   return (
     <>
       <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        {/* private route */}
         <Route path='/' element={<Home/>}/>
         <Route path='/quest' element={<Quest/>}/>
         <Route path='/quest/:id' element={<QuestRoutes/>}/>
         <Route path='/shop' element={<Shop/>}/>
+        {/* private route */}
         <Route path='*' element={<NotFound/>}/>
       </Routes>
     </>
