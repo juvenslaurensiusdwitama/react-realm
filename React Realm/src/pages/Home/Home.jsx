24caf1f091eee28e50611465d47798a7687132c5
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2025-06-03 18:08:31 +0700

    .

--- React Realm/src/pages/Home/Home.jsx
+++ React Realm/src/pages/Home/Home.jsx
@@ -8,10 +8,29 @@ const Home = () => {
             style={{ backgroundImage: `url(${bgHome})` }}
         >
             <Menu/>
-            <div className='my-8 bg-slate-900/80 h-[630px] w-[700px] backdrop-blur-xs 
-                rounded-[8px] text-white py-4 px-6'
+            <div className='my-8 bg-slate-900/80 h-[620px] w-[600px] backdrop-blur-xs 
+                rounded-[8px] text-white py-4 px-6 flex flex-col justify-between'
             >
-                tes
+                <div className='flex justify-between'>
+                    <div className='flex flex-col'>
+                        <h1 className='text-[18px]'>Username</h1>
+                        <div className='flex gap-2 text-[14px]'>
+                            <p>Level 10</p>
+                            <p>214 exp / 344 exp</p>
+                        </div>
+                        <p>Progress Bar</p>
+                    </div>
+                    <div>
+                        <p>Points</p>
+                        <p>Badge</p>
+                        <p>Thropy</p>
+                    </div>
+                </div>
+                <div className='w-full flex justify-around'>
+                    <p>char</p>
+                    <p>char</p>
+                </div>
+                <div>tes</div>
             </div>
         </div>
     )
--- React Realm/src/pages/Quest/components/Lesson.jsx
+++ React Realm/src/pages/Quest/components/Lesson.jsx
@@ -10,6 +10,10 @@ const Lesson = ({ data, loading }) => {
     if (loading) return <div>Loading...</div>;
     console.log(data)
     
+    const handleFinish = () =>{
+        
+    }
+
     return (
         <div
             className="h-screen bg-cover bg-center flex flex-col items-center"
@@ -19,12 +23,12 @@ const Lesson = ({ data, loading }) => {
             <div className='flex flex-col my-8 rounded-[16px] border-[4px] border-[#F6F8D5] 
                 border-solid w-[750px]'>
                 <div className='bg-[#205781] min-h-[370px] rounded-t-[12px] py-3 px-4'>
-                    <div className='flex justify-between text-[14px] text-[#efff94] font-semibold'>
+                    <div className='flex justify-between text-[14px] text-[#F6F8D5] font-semibold'>
                         <h1>{data.title}</h1>
                         <div className='flex gap-1'>
-                            <p className='text-[#F6F8D5]'>Reward:</p>
-                            <p className='text-[14px]'>{data.exp} exp</p>
-                            <p className='text-[#F6F8D5]'>|</p>
+                            <p>Reward:</p>
+                            <p className='text-[14px] text-[#efff94]'>{data.exp} exp</p>
+                            <p>|</p>
                             <BadgesValidation data={data?.badges}/>
                         </div>
                     </div>
@@ -33,16 +37,27 @@ const Lesson = ({ data, loading }) => {
                 <div className='flex bg-[#4F959D] 
                 min-h-[200px] rounded-b-[12px] py-3 pr-[80px] items-center'>
                     <img src={gandalf} alt="" className='h-[180px]'/>
-                    <div className='flex flex-col gap-3'>
-                        <p className='text-[#efff94] font-semibold'>{data.contents[contentIndex]}</p>
-                        <div className='flex gap-1 justify-end'>
-                            <img src={arrow} 
-                                className={`cursor-pointer rotate-180 h-[24px] transition-all ${contentIndex === 0 ? 'opacity-50 cursor-not-allowed' : 'hover:brightness-125'}`} 
-                                onClick={() => contentIndex > 0 && setContentIndex(contentIndex - 1)}
-                            />
-                            <img src={arrow} 
-                                className={`cursor-pointer h-[24px] transition-all ${contentIndex === data.contents.length - 1 ? 'opacity-50 cursor-not-allowed' : 'hover:brightness-125'}`} 
-                                onClick={() => contentIndex < data.contents.length - 1 && setContentIndex(contentIndex + 1)}/>
+                    <div className='flex flex-col gap-3 text-[#efff94] font-semibold'>
+                        <p>{data.contents[contentIndex]}</p>
+                        <div className='flex justify-end items-center gap-2'>
+                            <div className='flex gap-1'>
+                                <img src={arrow} 
+                                    className={`cursor-pointer rotate-180 h-[24px] transition-all ${contentIndex === 0 ? 'opacity-50 cursor-not-allowed' : 'hover:brightness-125'}`} 
+                                    onClick={() => contentIndex > 0 && setContentIndex(contentIndex - 1)}
+                                />
+                                <img src={arrow} 
+                                    className={`cursor-pointer h-[24px] transition-all ${contentIndex === data.contents.length - 1 ? 'opacity-50 cursor-not-allowed' : 'hover:brightness-125'}`} 
+                                    onClick={() => contentIndex < data.contents.length - 1 && setContentIndex(contentIndex + 1)}/>
+                            </div>
+                            {contentIndex === data.contents.length - 1 && 
+                                <button 
+                                    className='bg-[#205781] py-1 px-4 rounded-[10px] cursor-pointer transition duration-150
+                                    border-[2px] border-solid text-[#efff94] text-[14px] hover:opacity-[0.8]'
+                                    onClick={() => handleFinish()}
+                                >
+                                    Finish
+                                </button>
+                            }
                         </div>
                     </div>
                 </div>
