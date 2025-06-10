f3a7d1c03a504c5535a888f14c4ae97f3bfaa2bc
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2025-06-11 00:31:39 +0700

    fix bug on selecting char

--- React Realm/src/pages/Quest/components/QuestList.jsx
+++ React Realm/src/pages/Quest/components/QuestList.jsx
@@ -47,7 +47,7 @@ const QuestList = () => {
     return (
         <div className='my-8 flex items-center text-[#F6F8D5]'>
             <div className='bg-[#205781] rounded-[16px] border-[10px] border-[#F6F8D5] border-double
-            min-h-[500px] min-w-[450px] flex flex-col gap-4 items-center pt-7 pb-8 px-8
+            min-h-[250px] min-w-[450px] flex flex-col gap-4 items-center pt-7 pb-8 px-8
         '>
                 <div className='flex flex-col items-start w-full'>
                     <h1 className='text-[32px] font-medium'>Quest List</h1>
